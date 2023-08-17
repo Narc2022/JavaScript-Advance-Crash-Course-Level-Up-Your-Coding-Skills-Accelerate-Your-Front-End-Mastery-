13ef319ba529d9b6a359fc08536e52eada2123ec
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-08-17 09:52:15 +0530

    Create 5WindowObject.js

---
+++ 5WindowObject.js
@@ -0,0 +1,10 @@
+/*
+js mein kai saare features hai par kuchh features jo
+hum use karte hai wo features wo nahi hai par fir bhi
+use kar paate hai kyoki wo features js langauge use kar
+leti hai window se, aur window hai ek box of features
+given by browser to use with js
+
+br
+alert,prompt,localStorage,print,prompt & setTimeout
+*/
